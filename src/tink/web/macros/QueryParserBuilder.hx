4a01cace3a6f85d05bac321f1ed841ef091cc5e8
kevinresol <kevinresol@gmail.com>
2016-06-07 20:18:10 +0800

    Cast to String explicitly for it to work in case of Array<String>

--- src/tink/web/macros/QueryParserBuilder.hx
+++ src/tink/web/macros/QueryParserBuilder.hx
@@ -55,7 +55,7 @@ class QueryParserBuilder {
   static public function string():Expr 
     return 
       macro 
-        if (exists[prefix]) params[prefix];
+        if (exists[prefix]) (params[prefix]:String);
         else missing(prefix);
     
   static public function float():Expr
