9d06a07f542c69a834699ae7adbc3ee3c5702cf1
Juraj Kirchheim <back2dos@googlemail.com>
2016-12-06 10:39:25 +0100

    Actually add the proxy test.

---
+++ tests/ProxyTest.hx
@@ -0,0 +1,28 @@
+package;
+
+import deepequal.DeepEqual;
+import haxe.unit.TestCase;
+import tink.http.Client;
+import tink.http.Response.OutgoingResponse;
+import tink.http.containers.LocalContainer;
+import tink.url.Host;
+import tink.web.proxy.Remote;
+
+using tink.CoreApi;
+
+class ProxyTest extends TestCase {
+
+  function testProxy() {
+    var c = new LocalContainer();
+    var client = new LocalContainerClient(c);
+    var f = new Fake();
+    
+    c.run(function (req) {
+      return DispatchTest.exec(req).recover(OutgoingResponse.reportError);
+    });
+    
+    var p = new tink.web.proxy.Remote<Fake>(client, new RemoteEndpoint(new Host('localhost', 80)));
+    var c:Fake.Complex = { foo: [ { z: 3, x: '5', y: 6 } ] };
+    p.complex(c).handle(function (o) assertEquals(Noise, DeepEqual.compare(c, o.sure()).sure()));
+  }
+}
