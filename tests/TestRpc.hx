f93fd00a895d191e76d4e04de17afb8481fc59b1
Juraj Kirchheim <back2dos@googlemail.com>
2018-05-24 17:56:55 +0200

    Indenting [skip ci]

--- tests/TestRpc.hx
+++ tests/TestRpc.hx
@@ -1,17 +1,17 @@
 @:asserts
 class TestRpc {
-	public function new() {}
-	@:variant('/?action=foo&bar=123', '{"bar":123}')
-	@:variant('/?action=bar&foo=1', '{"foo":true}')
+  public function new() {}
+  @:variant('/?action=foo&bar=123', '{"bar":123}')
+  @:variant('/?action=bar&foo=1', '{"foo":true}')
   public function rpc(url:String, result:String) {
-		var r = new tink.web.routing.Router<Rpcesque>(new Rpcesque());
-		return check(
-			r.route(tink.web.routing.Context.ofRequest(get(url))),
-			'$url should return $result', 
-			function (s) {
-				return s == result;
-			}
-		);
+    var r = new tink.web.routing.Router<Rpcesque>(new Rpcesque());
+    return check(
+      r.route(tink.web.routing.Context.ofRequest(get(url))),
+      '$url should return $result', 
+      function (s) {
+        return s == result;
+      }
+    );
   }
 }
 
