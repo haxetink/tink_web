fb0f07dc1deff519cc5c18a578d4bd18a1be0b8d
Juraj Kirchheim <back2dos@googlemail.com>
2020-04-25 18:49:27 +0200

    Make it easier to pass content type to opaque responses.

--- src/tink/web/routing/Response.hx
+++ src/tink/web/routing/Response.hx
@@ -7,26 +7,34 @@ import tink.http.Header;
 
 using tink.io.Source;
 
-@:forward @:allow(tink.web)
+@:forward
 abstract Response(OutgoingResponse) from OutgoingResponse to OutgoingResponse {
-  
-  @:from static function ofString(s:String):Response 
+
+  static inline var BINARY = 'application/octet-stream';
+
+  @:from static function ofString(s:String):Response
     return textual('text/plain', s);
-  
-  @:from static function ofBytes(b:Bytes):Response 
-    return binary('application/octet-stream', b);
-  
-  @:from static function ofRealSource(source:RealSource):Response 
-    return new OutgoingResponse(new ResponseHeader(OK, OK, [new HeaderField(CONTENT_TYPE, 'application/octet-stream')]), source.idealize(function(_) return Source.EMPTY));
-    
-  @:from static function ofIdealSource(source:IdealSource):Response 
-    return new OutgoingResponse(new ResponseHeader(OK, OK, [new HeaderField(CONTENT_TYPE, 'application/octet-stream')]), source);
-    
+
+  @:from static function ofBytes(b:Bytes):Response
+    return binary(BINARY, b);
+
+  static public function ofRealSource(source:RealSource, ?contentType:String = BINARY):Response
+    return new OutgoingResponse(new ResponseHeader(OK, OK, [new HeaderField(CONTENT_TYPE, contentType)]), source.idealize(function(_) return Source.EMPTY));
+
+  static public function ofIdealSource(source:IdealSource, ?contentType:String = BINARY):Response
+    return new OutgoingResponse(new ResponseHeader(OK, OK, [new HeaderField(CONTENT_TYPE, contentType)]), source);
+
+  @:from static function fromRealSource(source:RealSource):Response
+    return ofRealSource(source.idealize(function(_) return Source.EMPTY));
+
+  @:from static function fromIdealSource(source:IdealSource):Response
+    return ofIdealSource(source);
+
   #if tink_template
   @:from static function ofHtml(h:tink.template.Html)
     return textual('text/html', h);
   #end
-  
+
   @:from static function ofUrl(u:tink.Url):Response {
     return new OutgoingResponse(new ResponseHeader(Found, Found, [new HeaderField('location', u)]), Chunk.EMPTY);
   }
@@ -35,11 +43,11 @@ abstract Response(OutgoingResponse) from OutgoingResponse to OutgoingResponse {
     //TODO: calculate ETag
     return OutgoingResponse.blob(code, bytes, contentType, headers);
   }
-  
+
   static public function empty(?code = OK):Response {
     return new OutgoingResponse(new ResponseHeader(code, code, [new HeaderField(CONTENT_LENGTH, '0')]), Chunk.EMPTY);
   }
-    
+
   static public function textual(?code, contentType:String, string:String, ?headers):Response
     return binary(code, contentType, Bytes.ofString(string), headers);
 }
