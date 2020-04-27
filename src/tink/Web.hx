ded77fae2511d017bb8f941fee46d68280d35656
Juraj Kirchheim <back2dos@googlemail.com>
2020-04-28 01:15:11 +0200

    Implement tink.Web.serve

---
+++ src/tink/Web.hx
@@ -0,0 +1,36 @@
+package tink;
+
+#if macro
+import haxe.macro.Expr;
+using tink.MacroApi;
+#end
+class Web {
+  static public macro function connect(e:Expr, ?options:ExprOf<tink.web.proxy.ConnectOptions>)
+    return switch e {
+      case macro ($url:$t), { expr: ENew(TPath(_) => t, [url])}:
+        var options = switch options {
+          case null | macro null: new Map();
+          case { expr: EObjectDecl(fields) }:
+            (macro @:pos(options.pos) ($options:tink.web.proxy.ConnectOptions)).typeof().sure();
+            [for (f in fields) f.field => f.expr];
+          case v:
+            v.reject('anonymous object expected');
+        }
+
+        var client = switch options['client'] {
+          case null: macro @:privateAccess tink.http.Fetch.getClient(Default, true);//not sure how adequate this is
+          case v: v;
+        }
+
+        var endpoint = macro @:pos(url.pos) tink.web.proxy.Remote.RemoteEndpoint.ofString($url);
+
+        switch options['headers'] {
+          case null:
+          case v:
+            endpoint = macro @:pos(v.pos) endpoint.sub({ headers: $v });
+        }
+
+        macro @:pos(e.pos) new tink.web.proxy.Remote<$t>($client, $endpoint);
+      default: e.pos.error('Expected (url:Type) or new Type(url)');
+    }
+}
